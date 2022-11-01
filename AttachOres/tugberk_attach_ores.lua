3a9b03a74324091209080d55e3f77ab9afc937fa
tugberk963 <102547030+tugberk963@users.noreply.github.com>
2022-11-02 00:56:07 +0300

    Update tugberk_attach_ores.lua

--- AttachOres/tugberk_attach_ores.lua
+++ AttachOres/tugberk_attach_ores.lua
@@ -1,8 +1,8 @@
 quest tugberk_cevher_npc begin
 	state start begin
-		when 9003.chat."Cevher Ekle" begin
-			say("Cevher eklemek istediğiniz nesneyi üzerime sürükleyin.")
-		end -- when 9006.chat end
+		when 9003.chat."Cevher Ekle" begin -- Change 9003 with NPC number you want.
+			say("Cevher eklemek istediÃ°iniz nesneyi Ã¼zerime sÃ¼rÃ¼kleyin.")
+		end -- when 9003.chat end
 		when 9003.take with item.get_sub_type() == 3 or item.get_sub_type() == 5 or item.get_sub_type() == 6 begin
 			item.set_socket(1, 21600)
 			item.set_socket(2, 21600)
@@ -10,7 +10,7 @@ quest tugberk_cevher_npc begin
 			item.set_socket(0, 1)
 			item.set_socket(0, 2)
 			item.set_socket(0, 3)
-			chat("Cevherler başarıyla eklendi. İyi Oyunlar.")
-		end -- when 9006.take end
+			chat("Cevherler baÃ¾arÃ½yla eklendi. Ãyi Oyunlar.")
+		end -- when 9003.take end
 	end -- state end
 end -- quest end
